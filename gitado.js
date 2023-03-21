440721e562ccdcd0ab18e9822cdd21506a8a4d20
lucashaetingerr <97047204+lucashaetingerr@users.noreply.github.com>
2023-03-21 20:35:59 -0300

    Add files via upload

--- gitado.js
+++ gitado.js
@@ -1,28 +1,24 @@
-function verificar(){
-    var val1 = document.getElementById('txt_valor1')
-    var val2 = document.getElementById('txt_valor2')
-    var verif = document.querySelector('div#verif')
-    var result = Number(val1.value-val2.value)
+function verificar() {
+    const val1 = document.getElementById('txt_valor1').value
+    const val2 = document.getElementById('txt_valor2').value
+    const verif = document.querySelector('div#verif')
 
-    if ( (result>=0) && ( (val1!=0) && (val2!=0) ) ){
-        console.log(result)
-        verif.innerHTML = (`Você pode comprar o produto. Seu novo saldo será de R$${result}`)
-    }else if(result<0){
-        console.log(result)
-        verif.innerHTML = `Você não pode comprar o produto`
-    }else{
-        console.log("Insira valores ao campo 1 e 2")
-        verif.innerHTML = "Insira valores ao campo 1 e 2"
+    if (val1 === "" || val2 === "") {
+        verif.innerHTML = "Por favor, insira valores em ambos os campos."
+    } else {
+        const result = val1 - val2
+        if (result >= 0) {
+            verif.innerHTML = `Você pode comprar o(s) produto(s). Seu novo saldo será de R$${result.toFixed(2)}.`
+        } else {
+            verif.innerHTML = "Você não pode comprar o(s) produto(s)."
+        }
     }
-
 }
 
-function limpar(){
-    var val1 = document.getElementById('txt_valor1')
-    var val2 = document.getElementById('txt_valor2')
-    var verif = document.querySelector('div#verif')
+function limpar() {
+    const verif = document.querySelector('div#verif')
     console.log("Limpando os campos da página...")
     verif.innerHTML = ""
-    document.getElementById("txt_valor1").value = null
-    document.getElementById("txt_valor2").value = null
+    document.getElementById("txt_valor1").value = ""
+    document.getElementById("txt_valor2").value = ""
 }
